cf8118c36fbcea01c2e415295745dfd6f1074793
yaxitdesai <134709393+yaxitdesai@users.noreply.github.com>
2023-05-31 18:36:35 +0530

    Add files via upload

---
+++ day8/tax1.c
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<stdio.h>
+
+int main(){
+	
+	int a ;
+	int b ;
+     printf("Enter velue of a :");
+     scanf("%d",&a);
+     
+     
+     printf("Enter velue of b :");
+     scanf("%d",&b);
+    
+	if(a>b){
+     
+	 printf("a is greterthen b :");	
+		
+	
+	}
+	
+	else {
+		
+		printf("a is lessthen b :");
+	}
+	
+	
+	
+	
+	
+}
---
+++ day8/tax2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+	int a ;
+	printf("Enter velue of a :");
+	scanf("%d",&a);
+	
+	
+	
+    if(a>0){
+    	
+    	printf("your velue is positive");
+    	
+    	
+	}	
+	 else if (a<0){
+	 	
+	 	printf("your velue is negative");
+	 	
+	 	
+	 }
+      
+       else { 
+       printf("your velue is neutral");
+	   }
+	 
+	 	
+	 	
+	 	
+	 }
+
